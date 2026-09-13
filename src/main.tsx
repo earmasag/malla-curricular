a60c8f976525ec005a069ad66c01cf5570ee20f0
earmasag <121563193+earmasag@users.noreply.github.com>
2026-09-13 11:54:30 -0400

    perf: precargar fuentes tipograficas para evitar FOUT

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource/oswald/latin-400.css'
-import '@fontsource/oswald/latin-500.css'
-import '@fontsource/oswald/latin-700.css'
 import './index.css'
 import App from './App.tsx'
 
